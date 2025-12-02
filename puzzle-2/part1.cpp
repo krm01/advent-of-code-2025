c9d6bce1df52e73e1fb7af4749855feb296e9ffc
kentonm <krm01@users.noreply.github.com>
2025-12-02 01:21:22 -0800

    cleanup

--- puzzle-2/part1.cpp
+++ puzzle-2/part1.cpp
@@ -2,33 +2,6 @@
 
 using std::cout;
 
-// incorrect logic. this finds repeats of any length, but the question
-// wanted a sequence of digits repeated TWICE.
-bool has_repeats_wrong(const std::string& value) {
-    // need at least 2 digits to repeat
-    if (value.size() < 2) return false;
-
-    // only need to check single-char repeat for prime lengths
-    if (is_prime(value.size())) {
-        return value == repeat(value[0], value.size());
-    }
-
-    for (i64 cycle_len = 1; cycle_len <= value.size() / 2; cycle_len++) {
-        auto [n_cycles, rem] = divrem(value.size(), cycle_len);
-
-        // skip any cycles that can't fully repeat
-        if (rem != 0) {
-            continue;
-        }
-
-        if (value == repeat(value.substr(0, cycle_len), n_cycles)) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 bool has_repeats(const std::string& value) {
     // need at least 2 digits to repeat
     if (value.size() < 2) return false;
@@ -56,6 +29,4 @@ int main() {
     }
 
     std::cout << "sum: " << sum << "\n";
-    /* ATTEMPTS */
-    // 20077272987 TOO HIGH
 }
